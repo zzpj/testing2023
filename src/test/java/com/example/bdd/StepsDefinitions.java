0c2c563d0be1f7eeadcd5ba5bcf736deb1cbe29a
zn <snowbird@gmx.com>
2023-03-11 17:16:58 +0100

    zmiana dependencji na te z bom'a

---
+++ src/test/java/com/example/bdd/StepsDefinitions.java
@@ -0,0 +1,21 @@
+package com.example.bdd;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+public class StepsDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
